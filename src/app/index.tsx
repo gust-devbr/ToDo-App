30e92ae1854f0de66f1eb90ad7e00fbfce6b54b7
Gustavo Oliv <calacogustavo@gmail.com>
2026-03-06 02:56:36 -0300

    feat: verificar atualizações automaticamente

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,22 @@
 import { router } from "expo-router";
+import * as Updates from 'expo-updates';
 import { useEffect } from "react";
 
 export default function RootPage() {
     useEffect(() => {
         router.push("/(stack)/home")
     }, [])
+
+    useEffect(() => {
+        async function updateApp() {
+            const update = await Updates.checkForUpdateAsync();
+
+            if (update.isAvailable) {
+                await Updates.fetchUpdateAsync();
+                await Updates.reloadAsync();
+            }
+        }
+
+        updateApp();
+    }, []);
 }
